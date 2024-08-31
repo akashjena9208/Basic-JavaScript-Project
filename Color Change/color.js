28bc7abfbfa5fba6fd91b0db6fd72d79323268b3
Akash Jena <145319322+akashjena9208@users.noreply.github.com>
2024-08-31 12:51:54 +0530

    Add files via upload

---
+++ Color Change/color.js
@@ -0,0 +1,34 @@
+const Button=document.querySelectorAll(".button");
+const Body=document.querySelector('body')
+
+/*Button.forEach(function(btn) {
+    btn.addEventListener('click',function(e) 
+    {
+        if (e.target.id === 'grey') {
+            Body.style.background = 'grey'
+        }
+        if (e.target.id === 'yellow') {
+            Body.style.background = 'yellow'
+        }
+        if (e.target.id === 'white') {
+            Body.style.background = 'white'
+        }
+        if (e.target.id === 'blue') {
+            Body.style.background = 'blue'
+        }
+        if (e.target.id === 'red') {
+            Body.style.background = 'red'
+        }
+        if (e.target.id === 'green') {
+            Body.style.background = 'green'
+        }
+        if (e.target.id === 'pink') {
+            Body.style.background = 'pink'
+        }
+        if (e.target.id === 'orange') {
+            Body.style.background = 'blue'
+        }
+    })
+    
+});
+*/
